ce3a5cdf96ee7790d661fe07209f835544cc0460
assoares383 <alexandressoares383@gmail.com>
2020-07-26 15:52:42 -0300

    Challenge finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: any;
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,40 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (totalBalance: Balance, transaction: Transaction) => {
+        if (transaction.type === 'income') {
+          totalBalance.income += transaction.value;
+        }
+
+        if (transaction.type === 'outcome') {
+          totalBalance.outcome += transaction.value;
+        }
+
+        return totalBalance;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
